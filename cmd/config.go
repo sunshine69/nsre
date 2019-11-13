2ee3d3a235ede4aedb17181777e07d386f93d96b
Steve Kieu <steve.kieu@xvt.com.au>
2019-11-13 13:34:40 +1000

    Multiline log parser support completed.

--- cmd/config.go
+++ cmd/config.go
@@ -16,7 +16,8 @@ type LogFile struct {
     Timelayout string //Parse the match below into go time object
     Timepattern string //extract the timestamp part into a timeStr which is fed into the Timelayout
 	Timeadjust string //If the time extracted string miss some info (like year or zone etc) this string will be appended to the string
-	Pattern string //will be matched to extract the HOSTNAME APP-NAME MSG part of the line.
+    Pattern string //will be matched to extract the HOSTNAME APP-NAME MSG part of the line.
+    Multilineptn string //detect if the line is part of the previous line
 }
 //AppConfig -
 type AppConfig struct { //Why do I have to tag every field! Because yaml driver automatically lowercase the field name to look into the yaml file <yuk>
@@ -52,9 +53,10 @@ logfiles:
     - name: syslog
       path: /var/log/syslog
       timelayout: "Jan 02 15:04:05 2006 MST"
-      timepattern: '([a-zA-Z]{3,3} [\d]{0,2} [\d]{2,2}\:[\d]{2,2}\:[\d]{2,2}) '
+      timepattern: '^([a-zA-Z]{3,3} [\d]{0,2} [\d]{2,2}\:[\d]{2,2}\:[\d]{2,2}) '
       timeadjust: "2019 AEST"
       pattern: '([^\s]+) ([^\s]+) (.*)$'
+      multilineptn: '^[\s\t]+([^\s]+.*)$'
 `
     err := ioutil.WriteFile(fPath, []byte(defaultConfig), 0600)
     if err != nil {return err}
--- cmd/log.go
+++ cmd/log.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"io"
+
 	"sync"
 	"strings"
 	"bytes"
@@ -46,49 +48,106 @@ func TailLog(cfg *TailLogConfig, wg *sync.WaitGroup){
 	// 	syscall.SIGTERM,
 	// 	syscall.SIGQUIT)
 
-	ProcessLines(cfg, t.Lines)
+	ProcessLines(cfg, t)
 	// s := <-c
 	// log.Print(s.String())
 	// t.Stop()
 	wg.Done()
 }
-//ProcessLines -
-func ProcessLines(cfg *TailLogConfig, tailLines chan *tail.Line) {
 
-	timePtn := regexp.MustCompile(cfg.Timepattern)
-	linePtnStr := fmt.Sprintf("%s%s", cfg.Timepattern, cfg.Pattern)
-	linePtn := regexp.MustCompile(linePtnStr)
-	log.Printf("time ptn: '%s'\nline ptn: '%s'\n", cfg.Timepattern, linePtnStr)
+//IsEOF - NOt sure why tail does not provide this test.
+func IsEOF(filename string, seek int64) (bool) {
+	fh, e := os.Open(filename)
+	if e != nil {
+		fmt.Printf("ERROR can not open file - %v\n", e)
+	}
+	buff := make([]byte, 1)
+	fh.Seek(seek, 0)
+	_, e = fh.Read(buff)
+	if e == io.EOF {
+		fmt.Printf("ERROR\n")
+		return true
+	}
+	return false
+}
 
-	timeLayout := cfg.Timelayout
-
-	// lineStack := []string{}
-	var timeParsed time.Time
-	var e error
-
+//SendLine -
+func SendLine(timeParsed time.Time, hostStr, appNameStr, msgStr string) {
+	logData := LogData{
+		Timestamp: time.Now().UnixNano(),
+		Datelog: timeParsed.UnixNano(),
+		Host: hostStr,
+		Application: appNameStr,
+		Message: msgStr,
+	}
 	var json = jsoniter.ConfigCompatibleWithStandardLibrary
-
+	output, e := json.Marshal(&logData)
+	if e != nil {
+		log.Fatalf("ERROR - can not marshal json to output - %v\n", e)
+	}
 	client := &http.Client{}
     validToken, err := GenerateJWT()
     if err != nil {
         fmt.Println("Failed to generate token")
 	}
+	req, _ := http.NewRequest("POST", fmt.Sprintf("%s/log", Config.Serverurl), bytes.NewBuffer(output))
+	req.Header.Set("Token", validToken)
+	req.Header.Set("Content-Type", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+	}
+	_, err = ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+//ProcessLines -
+func ProcessLines(cfg *TailLogConfig, tail *tail.Tail) {
+	tailLines := tail.Lines
+	timePtn := regexp.MustCompile(cfg.Timepattern)
+	linePtnStr := fmt.Sprintf("%s%s", cfg.Timepattern, cfg.Pattern)
+	linePtn := regexp.MustCompile(linePtnStr)
+	multiLinePtn := regexp.MustCompile(cfg.Multilineptn)
+	log.Printf("time ptn: '%s'\nline ptn: '%s'\n", cfg.Timepattern, linePtnStr)
+
+	timeLayout := cfg.Timelayout
+
+	var timeParsed time.Time
+	var e error
+	var hostStr, appNameStr string
+
+	lineStack := []string{}
+	beginLineMatch := false
 
 	for line := range tailLines {
-
+		curSeek, _ := tail.Tell()
+		if IsEOF(tail.Filename, curSeek) {
+			msgStr := strings.Join(lineStack, "\n")
+			lineStack = lineStack[:0]
+			// log.Printf("EOF reached. Flush stack\n")
+			SendLine(timeParsed, hostStr, appNameStr, msgStr)
+		}
 		match := timePtn.FindStringSubmatch(line.Text)
 		if len(match) > 0 {
+			beginLineMatch = true
+			if len(lineStack) > 0 {//Flush the multiline stack
+				msgStr := strings.Join(lineStack, "\n")
+				lineStack = lineStack[:0]
+				SendLine(timeParsed, hostStr, appNameStr, msgStr)
+			}
 			timeStr := fmt.Sprintf("%s %s", match[1], cfg.Timeadjust)
 			timeStr = strings.Replace(timeStr, "  ", " ", -1)
 			timeParsed, e = time.Parse(timeLayout, timeStr)
 			if e != nil {
 				log.Fatalf("ERROR Fail to parse time %v\n", e)
 			}
-
 			match1 := linePtn.FindStringSubmatch(line.Text)
 			matchCount := len(match1)
 			if matchCount > 0 {
-				var hostStr, appNameStr, msgStr string
+				var msgStr string
 				switch matchCount {
 				case 3:
 					curHostname, _ := os.Hostname()
@@ -98,37 +157,29 @@ func ProcessLines(cfg *TailLogConfig, tailLines chan *tail.Line) {
 				case 5:
 					hostStr, appNameStr, msgStr = match1[2], match1[3], match1[4]
 				}
-
-				logData := LogData{
-					Timestamp: time.Now().UnixNano(),
-					Datelog: timeParsed.UnixNano(),
-					Host: hostStr,
-					Application: appNameStr,
-					Message: msgStr,
-				}
-				output, e := json.Marshal(&logData)
-				if e != nil {
-					log.Fatalf("ERROR - can not marshal json to output - %v\n", e)
-				}
-
-				req, _ := http.NewRequest("POST", fmt.Sprintf("%s/log", Config.Serverurl), bytes.NewBuffer(output))
-				req.Header.Set("Token", validToken)
-				req.Header.Set("Content-Type", "application/json")
-
-				res, err := client.Do(req)
-				if err != nil {
-					fmt.Printf("Error: %v", err)
-				}
-				_, err = ioutil.ReadAll(res.Body)
-				if err != nil {
-					fmt.Println(err)
+				if len(lineStack) == 0 {
+					lineStack = append(lineStack, msgStr)
 				}
 			} else {
 				log.Fatalf("The pattern does not parse correct components. You need to have capture groups - TIMESTAMP HOSTNAME APP-NAME MSG\n")
 			}
 		} else {
-			fmt.Printf("Line Text: '%s'\n", line.Text)
-			log.Printf("Can not parse the time pattern\n")
+			if beginLineMatch {
+				mMatch := multiLinePtn.FindStringSubmatch(line.Text)
+				if len(mMatch) > 0 {
+					if len(lineStack) > 0 {
+						// fmt.Printf("Part of multiLine Text: '%s'\n", line.Text)
+						lineStack = append(lineStack, mMatch[1])
+					}
+				} else {
+					beginLineMatch = false
+					log.Printf("Can not parse multiline pattern\n")
+					fmt.Printf("Line Text: '%s'\n", line.Text)
+				}
+			} else {
+				log.Printf("Can not parse time pattern\n")
+				fmt.Printf("Line Text: '%s'\n", line.Text)
+			}
 		}
 	}
 }
