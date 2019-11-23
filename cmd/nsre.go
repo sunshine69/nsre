811ae0eb03014ea4764ea256849c5521929d2fcf
Steve Kieu <steve.kieu@xvt.com.au>
2019-11-23 20:54:54 +1000

    Enable content compression

--- cmd/nsre.go
+++ cmd/nsre.go
@@ -15,6 +15,7 @@ import (
 	"github.com/gorilla/mux"
 	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/json-iterator/go"
+	"github.com/gorilla/handlers"
 )
 
 func isAuthorized(endpoint func(http.ResponseWriter, *http.Request)) http.Handler {
@@ -377,7 +378,7 @@ func HandleRequests() {
         WriteTimeout: time.Second * 15,
         ReadTimeout:  time.Second * 15,
         IdleTimeout:  time.Second * 60,
-        Handler: router, // Pass our instance of gorilla/mux in.
+        Handler: handlers.CompressHandler(router), // Pass our instance of gorilla/mux in.
     }
 
 	if Config.Sslkey != "" {
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/bvinc/go-sqlite-lite v0.6.1
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gorilla/handlers v1.4.2
 	github.com/gorilla/mux v1.7.3
 	github.com/gorilla/sessions v1.2.0
 	github.com/json-iterator/go v1.1.8
