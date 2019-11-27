1a01974c6b26496e49d9c04f8f93700bebe302af
Steve Kieu <steve.kieu@xvt.com.au>
2019-11-27 16:42:03 +1000

    Put some debugging in function

--- cmd/utils.go
+++ cmd/utils.go
@@ -41,6 +41,7 @@ func ParseTimeRange(durationStr, tz string) (time.Time, time.Time) {
 	timerangePtn := regexp.MustCompile(`([\d]{2,2}/[\d]{2,2}/[\d]{4,4} [\d]{2,2}:[\d]{2,2}:[\d]{2,2}) - ([\d]{2,2}/[\d]{2,2}/[\d]{4,4} [\d]{2,2}:[\d]{2,2}:[\d]{2,2})`)
 	dur, e := time.ParseDuration(durationStr)
 	if e != nil {
+		log.Printf("ERROR can not parse durection string using time.ParseDuration for %s - %v. Will try next\n", durationStr, e)
 		m := timerangePtn.FindStringSubmatch(durationStr)
 		if len(m) != 3 {
 			log.Printf("ERROR Can not parse duration. Set default to 15m ago - %v", e)
