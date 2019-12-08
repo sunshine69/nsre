4e42e1f8c224a51dacdb3434a919b7a8c37df9ea
Steve Kieu <steve.kieu@xvt.com.au>
2019-12-09 09:43:40 +1000

    Minor improvement

--- build-static.sh
+++ build-static.sh
@@ -7,6 +7,6 @@
 VER=$(git rev-parse --short HEAD)
 sed -i "s/const Version = .*/const Version = \"${VER}\"/" cmd/version.go
 
-docker run --rm -v $(pwd):/work --workdir /work --entrypoint go nsre-alpine-build build --tags "icu json1 fts5 secure_delete" --ldflags '-extldflags "-static" -w -s'
+docker run --rm -v $(pwd):/work --workdir /work --entrypoint go nsre-alpine-build:latest build --tags "icu json1 fts5 secure_delete" --ldflags '-extldflags "-static" -w -s' -o nsre-linux-amd64-static
 
-mv nsre ~/Public/nsre-static-alpine
+mv nsre-linux-amd64-static ~/Public/nsre-linux-amd64-static
