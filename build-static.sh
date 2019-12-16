d6a3d48851a5f2f224a117b3b3d0711f5ec0c9fa
Steve Kieu <steve.kieu@xvt.com.au>
2019-12-16 13:12:29 +1000

    Update build script to sue geenric docker image

--- build-static.sh
+++ build-static.sh
@@ -7,6 +7,8 @@
 VER=$(git rev-parse --short HEAD)
 sed -i "s/const Version = .*/const Version = \"${VER}\"/" cmd/version.go
 
-docker run --rm -v $(pwd):/work --workdir /work --entrypoint go nsre-alpine-build:latest build --tags "icu json1 fts5 secure_delete" --ldflags '-extldflags "-static" -w -s' -o nsre-linux-amd64-static
+#docker run --rm -v $(pwd):/work --workdir /work --entrypoint go nsre-alpine-build:latest build --tags "icu json1 fts5 secure_delete" --ldflags '-extldflags "-static" -w -s' -o nsre-linux-amd64-static
+
+docker run --rm -v $(pwd):/work --workdir /work --entrypoint go --env-file ~/.gobuild-linux-cgo golang-alpine-build:latest build --tags "json1 secure_delete" --ldflags '-extldflags "-static" -w -s' -o nsre-linux-amd64-static main.go
 
 mv nsre-linux-amd64-static ~/Public/nsre-linux-amd64-static
