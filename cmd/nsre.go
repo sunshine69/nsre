c75f3aad9e339b8151157e5c2734b38c2d97cf2a
Steve Kieu <steve.kieu@xvt.com.au>
2020-03-20 16:31:33 +1000

    Split sns functionality into its own file

--- cmd/nsre.go
+++ cmd/nsre.go
@@ -5,7 +5,6 @@ import (
 	"golang.org/x/crypto/acme/autocert"
 	"golang.org/x/crypto/acme"
 	"errors"
-	"net/http/httputil"
 	"mime/multipart"
 	"crypto/subtle"
 	"bufio"
@@ -538,53 +537,6 @@ func DoUploadLog(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, msg)
 		return
 	}
-}
-
-// For debugging purposes only. The endpoint handler can temporary call this to examine the data structure
-func SaveDumpData(host, application, logfile, message string) *LogData {
-	host = Ternary(host == "", "DEBUG", host).(string)
-	application = Ternary(application == "", "DUMPER", application).(string)
-
-	logData := LogData{
-		Timestamp: time.Now().UnixNano(),
-		Datelog: time.Now().UnixNano(),
-		Host: host,
-		Application: application,
-		Logfile: logfile,
-		Message: message,
-	}
-	data, _ := json.Marshal(logData)
-	InsertLog(data)
-	return &logData
-}
-
-func DumpPost(w http.ResponseWriter, r *http.Request) {
-	requestDump, err := httputil.DumpRequest(r, true)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Printf("DEBUG - DUMP\n\n%s\n",requestDump)
-	msg, _ := ioutil.ReadAll(r.Body)
-	fmt.Printf("DEBUG - Body\n\n%s\n",msg)
-
-	SaveDumpData("", "", r.Method, string(msg))
-
-	r.Body = ioutil.NopCloser(bytes.NewBuffer(msg))
-	fmt.Fprintf(w, "OK")
-	return
-}
-
-//HandleSNSEvent - From/To/action
-func HandleSNSEvent(w http.ResponseWriter, r *http.Request) {
-	msg, _ := ioutil.ReadAll(r.Body)
-	SaveDumpData("HandleSNSEvent", "twilio_call", r.Method, string(msg))
-	vars := mux.Vars(r)
-	From, To, Action := vars["from"], vars["to"], vars["action"]
-	Subj := json.Get(msg, "Subject").ToString()
-	Subj = Ternary(Subj == "", "no subject", Subj).(string)
-	Body := json.Get(msg, "Message").ToString()
-	Body = Ternary(Body == "", string(msg), Body).(string)
-	MakeTwilioCall("", Action, Body, From, To, "HandleSNSEvent", Subj, "")
 }
 
 //HandleRequests -
